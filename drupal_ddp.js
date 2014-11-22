b8f9d9a46e93ff4ec7e3a3c7563132adc8dc01e2
Mark Shropshire <marks@knowclassic.com>
2014-11-21 20:40:54 -0500

    More renaming

--- drupal_ddp.js
+++ drupal_ddp.js
@@ -6,11 +6,11 @@
 Drupal.behaviors.vertical_tabs_exampleFieldsetSummaries = {
   attach: function (context) {
     // Use the fieldset class to identify the vertical tab element
-    $('fieldset#edit-ddp-connect', context).drupalSetSummary(function (context) {
+    $('fieldset#edit-drupal-ddp', context).drupalSetSummary(function (context) {
       // Depending on the checkbox status, the settings will be customized, so
       // update the summary with the custom setting textfield string or a use a
       // default string.
-      if ($('#edit-ddp-connect-enabled', context).attr('checked')) {
+      if ($('#edit-drupa-ddp-enabled', context).attr('checked')) {
         return Drupal.t('Use with DDP');
       }
       else {
