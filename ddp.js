d53c6b3f731c5e9596648f3d6fce681841e9cc64
Bayo Fodeke <bfodeke@gmail.com>
2015-10-06 00:00:30 -0400

    Sending meteor's response back to Drupal.
    Added more debugging and console output to give more context to what is currently happening.

--- ddp.js
+++ ddp.js
@@ -11,12 +11,12 @@ var DDPClient = require("ddp");
 var app = express();
 
 var ddpclient = new DDPClient({
-  // host: "localhost",
+  host: "localhost",
   port: 3000,
-  // autoReconnect: true,
-  // autoReconnectTimer: 500,
-  // ddpVersion: '1',
-  // ssl: false,
+  autoReconnect: true,
+  autoReconnectTimer: 500,
+  ddpVersion: '1',
+  ssl: false,
   maintainCollections: false,
   useSockJs: true,
 });
@@ -32,27 +32,42 @@ app.post('/', function(req, res, next) {
   console.log(req.headers);
   console.log(req.body);
 
-  ddpclient.call(
-    'DrupalSaveNode',
-    [req.body]
-  );
+  ddpclient.call('DrupalSaveNode', [req.body], function(err, response) {
+    if (!err) {
+      res.send(response);
+    } else {
+      res.send(err);
+    }
+  });
 
-  res.send({ status: 'Post successfully sent to Meteor!' });
+
 });
 
-ddpclient.connect(function(err) {
-  if (err) {
-    throw new Error('DDP connection error!');
+ddpclient.connect(function(error, wasReconnect) {
+  // If autoReconnect is true, this callback will be invoked each time
+  // a server connection is re-established
+  if (error) {
+    console.log('DDP connection error!');
+    return;
   }
-  console.log('Connected!');
+
+  if (wasReconnect) {
+    console.log('Re-establishment of a connection.');
+  }
+
+  console.log('connected!');
+});
+
+// Monitor ddp connection for messages (for debugging purposes).
+ddpclient.on('message', function (msg) {
+  console.log("ddp message: " + msg);
 });
 
 // Nodejs listener setup for port 8080.
 app.listen(8080);
 console.log('Listening for data on port: 8080');
 
-
-// Monitor ddp connection for messages (for debugging purposes).
-ddpclient.on('message', function (msg) {
-  console.log("ddp message: " + msg);
+// Display when socket is closed.
+ddpclient.on('socket-close', function(code, message) {
+  console.log("Close: %s %s", code, message);
 });
